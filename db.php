5563df8cd0476ce323449cfdc9fe01d8ec38f8bd
Renato Cardoso <re2005@gmail.com>
2018-05-03 23:13:36 +0200

    add db

---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+
+	include_once('pass.php');
+
+	// Set here your credentials
+	// $dbhost = "";
+	// $dbuser = "";
+	// $dbpass = "";
+
+
+	$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+	if(! $conn )
+	{
+		die('Could not connect: ' . mysql_error());
+	}
+	$db = mysql_select_db('formapp2014');
+	mysql_set_charset('utf8');
+
+	$usertable = "formapp";
+?>
